64f495babfb28b6fac2b23c65eab5f298c5f678e
Dylan Renwick <skidsirongenesisdev@gmail.com>
2018-07-27 14:19:38 +0100

    Reworked logger to provide cleaner output

--- log.js
+++ log.js
@@ -1,38 +1,42 @@
 const fs = require('fs');
 
+const colors = {
+	error: '\x1b[31m',
+	success: '\x1b[32m',
+	warning: '\x1b[33m',
+	debug: '\x1b[33m',
+	reset: '\x1b[0m'
+}
+
 const logLevels = [
-	'EMERGENCY',
-	'CRITICAL',
-	'ALERT',
-	'ERROR',
-	'WARNING',
-	'NOTICE',
-	'INFO',
-	'DEBUG'
+	'Error',
+	'Success',
+	'Warning',
+	'Debug',
+	'Info'
 ];
 
-module.exports = {
-	log: log,
-	LEVEL_EMERGENCY: 0,
-	LEVEL_CRITICAL: 1,
-	LEVEL_ALERT: 2,
-	LEVEL_ERROR: 3,
-	LEVEL_WARNING: 4,
-	LEVEL_NOTICE: 5,
-	LEVEL_INFO: 6,
-	LEVEL_DEBUG: 7
-};
+exports.log = log;
 
-function log() {
-	let args = Array.from(arguments);
+for(let i = 0; i < logLevels.length; i++) {
+	module.exports[logLevels[i].toLowerCase()] = (...args) => {
+		args.push(i);
+		return log(...args);
+	}
+}
+
+function log(...args) {
 	let logLevel = args.pop();
 	let message = '';
-
-	for(; args.length > 0;) {
-		let arg = args.shift.toString()
-		message += arg + (args.length >= 1 ? ' ' : '');
+	while(args.length > 0) {
+		let arg = args.shift();
+		let strArg
+		if (arg === null) strArg = 'null';
+		else if (arg === undefined) strArg = 'undefined';
+		else strArg = arg.toString();
+		message += strArg + (args.length >= 1 ? ' ' : '');
 	}
-	
+
 	let date = new Date();
 	let year = padString(date.getFullYear(), 4, '0');
 	let month = padString(date.getMonth() + 1, 2, '0');
@@ -40,12 +44,14 @@ function log() {
 	let hour = padString(date.getHours(), 2, '0');
 	let minute = padString(date.getMinutes(), 2, '0');
 	let second = padString(date.getSeconds(), 2, '0');
+	let ms = padString(date.getMilliseconds().toString().substring(0, 3), 3, '0', false);
 
-	let timestamp = '[' + year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second + ']';
+	let timestamp = '[' + year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second + '.' + ms + ']';
 
-	let warnLabel = padString('[' + logLevels[logLevel] + ']', 11, ' ', false);
+	let levelLabel = padString(' ' + logLevels[logLevel], 11, ' ', false);
+	if (colors[logLevels[logLevel].toLowerCase()]) levelLabel = colors[logLevels[logLevel].toLowerCase()] + levelLabel + colors.reset;
 
-	message = timestamp + warnLabel + '  ' + message;
+	message = timestamp + levelLabel + ' | ' + message;
 
 	console.log(message);
 }
